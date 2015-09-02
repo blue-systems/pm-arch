ca5eb4dc2f31651ed62e08398b90e473d8578d29
Bhushan Shah <bhush94@gmail.com>
2015-09-02 17:51:36 +0530

    have custom sudoers file

--- airootfs/root/customize_airootfs.sh
+++ airootfs/root/customize_airootfs.sh
@@ -19,6 +19,8 @@ chmod 700 /root
 chown -R root:root /root
 chown -R root:root /etc
 
+chmod 440 /etc/sudoers
+
 sed -i 's/#\(PermitRootLogin \)no/\1yes/' /etc/ssh/sshd_config
 sed -i "s/#Server/Server/g" /etc/pacman.d/mirrorlist
 sed -i 's/#\(Storage=\)auto/\1volatile/' /etc/systemd/journald.conf
